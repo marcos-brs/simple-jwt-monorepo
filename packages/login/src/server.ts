f8c7a00853011f174d3903cbfb90a64ba5494f81
Marcos Santana <brmarcossantanabr@gmail.com>
2020-09-15 21:50:08 -0300

    feat: create login route

--- packages/login/src/server.ts
+++ packages/login/src/server.ts
@@ -2,3 +2,14 @@ import express from 'express';
 
 const app = express();
 app.use(express.json());
+
+const routes = express.Router();
+
+routes.get('/login', (req, res) => {
+  res.json({ message: 'login page' });
+});
+
+app.use(routes);
+app.listen(3333, () => {
+  console.log('App running on 3333');
+});
