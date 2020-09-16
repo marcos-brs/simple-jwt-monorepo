bf3149ce427707631de2818c81ea4be4be6c389c
Marcos Santana <brmarcossantanabr@gmail.com>
2020-09-15 21:57:22 -0300

    feat: create token on /login route

--- packages/login/src/server.ts
+++ packages/login/src/server.ts
@@ -1,15 +1,25 @@
 import express from 'express';
+import AuthService from './services/AuthService';
 
 const app = express();
 app.use(express.json());
 
 const routes = express.Router();
 
-routes.get('/login', (req, res) => {
-  res.json({ message: 'login page' });
+routes.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  const authService = new AuthService();
+
+  const token = authService.execute({ email, password });
+
+  return res.json({
+    token,
+  });
 });
 
 app.use(routes);
+
 app.listen(3333, () => {
   console.log('App running on 3333');
 });
